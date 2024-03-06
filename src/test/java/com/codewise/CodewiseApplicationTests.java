6d09ce3f3bb1c86c38ba4a466d11883f437e5eae
codewiser116 <benazir.baialieva@gmail.com>
2024-03-06 14:45:45 -0600

    first commit

---
+++ src/test/java/com/codewise/CodewiseApplicationTests.java
@@ -0,0 +1,14 @@
+package com.codewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodewiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("This is test!");
+	}
+
+}
